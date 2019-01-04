2c95dea7161d205e1b2c14e649f0340a94423427
Kelsey Low <kklowlow@gmail.com>
2019-01-04 05:23:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    return array
+    return array.join
   elsif array.length == 2
-    array.last.prepend "and "
+    array.join "and "
   elsif array.length > 2 
     array.join ", "
     array.last.prepend "and "
